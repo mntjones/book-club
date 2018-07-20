ba168638562cd6d3b9bb8a72124e7dc24c2d7625
Monica <mntamaru@gmail.com>
2018-07-20 15:31:20 -0700

    initial models and migrations

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+	belongs_to :user
+	belongs_to :book
+
+	def avg_rating
+		
+	end
+	
+end
---
+++ db/migrate/20180720222345_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+
+      t.string :comments
+      t.integer :rating
+      t.integer :user_id
+      t.integer :book_id
+      
+    end
+  end
+end
