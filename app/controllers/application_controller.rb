4aa4b6dc78212dc7fc89e1b61cf97bf9f4b222a3
Monica <mntamaru@gmail.com>
2018-07-20 15:40:53 -0700

    initial controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+	def logged_in?
+	    redirect_to '/' unless current_user
+	end
+
+	def current_user
+	  User.find_by(id: session[:user_id])
+	end
+
+
 end
