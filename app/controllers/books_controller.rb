01ec53f017cb9bbd685a188fbfa244f7b4666fe1
Monica <mntamaru@gmail.com>
2018-07-21 09:52:17 -0700

    new book form completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,19 +2,34 @@ class BooksController < ApplicationController
 
 	def new
 		@grouped_options = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
+		@book = Book.new
 	end
 
 	def create
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to book_path(@book)
+		else
+			redirect_to new_book_path, alert: "Information entered incorrectly, please try again."
+		end
 
 	end
 
 	def show
+		@book = Book.find_by_id(params[:id])
 
 	end
 
 	def index
+		@books = Book.all
 
+	end
+
+	private
+	def book_params
+
+		params.require(:book).permit(:title, :author, :genre, :pages, :month_assigned, :year_assigned)
 	end
 
 end
