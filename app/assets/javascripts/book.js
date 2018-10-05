5fbc2366c5cde3d4955ac740abda42eeea2d9f2e
Monica <mntamaru@gmail.com>
2018-10-05 14:34:51 -0700

    moved all js to book.js

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,16 +1,35 @@
 // setting up a method to show all book reviews when button is clicked.
 class Book {
 	constructor(book) {
-		this.id = book["id"];
-		this.title = book["title"];
-		this.author = book["author"];
-		this.month_assigned = book["month_assigned"];
-		this.year_assigned = book["year_assigned"];
-		this.genre = book["genre"];
-		this.pages = book["pages"];
 
-		if (book["reviews"]) {
-			console.log("I have reviews");
-		}
+		//debugger
+		this.id = book["id"];
+		this.title = book["attributes"]["title"];
+		this.author = book["attributes"]["author"];
+		this.month_assigned = book["attributes"]["month-assigned"];
+		this.year_assigned = book["attributes"]["year-assigned"];
+		this.genre = book["attributes"]["genre"];
+		this.pages = book["attributes"]["pages"];
+
+		this.reviews = book["attributes"]["reviews"]
+	}
+
+	showReviews() {
+		//loop over this.reviews
 	}
 }
+
+
+$(function () {
+  $(".js-reviews").on('click', function() {
+    var id = $(this).data("id");
+    $.get("/books/" + id + '.json', function(data) {
+      // load reviews
+      var book = new Book(data.data);
+      //debugger
+      // book methods data.attributes.reviews - loop over reviews, format to html
+      // book.showReviews
+      $("#reviews-" + id).text(data);
+    });
+  });
+});
