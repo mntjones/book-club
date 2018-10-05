6b222b4eb599bda85a05be39f947a01966c0cb16
Monica <mntamaru@gmail.com>
2018-10-04 22:04:54 -0700

    serializer updated to fix nested resource problem

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,1 +1,16 @@
-book.js
+// setting up a method to show all book reviews when button is clicked.
+class Book {
+	constructor(book) {
+		this.id = book["id"];
+		this.title = book["title"];
+		this.author = book["author"];
+		this.month_assigned = book["month_assigned"];
+		this.year_assigned = book["year_assigned"];
+		this.genre = book["genre"];
+		this.pages = book["pages"];
+
+		if (book["reviews"]) {
+			console.log("I have reviews");
+		}
+	}
+}
--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,5 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :genre, :pages, :month_assigned, :year_assigned
+  attributes :id, :title, :author, :genre, :pages, :month_assigned, :year_assigned, :reviews
   has_many :reviews
  
 end
